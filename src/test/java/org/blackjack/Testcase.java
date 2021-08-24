98c4398ba84b027ebf0937b696e04b4af53b05e3
Komal0210 <89078874+Komal0210@users.noreply.github.com>
2021-08-24 15:51:45 +0100

    Update Testcase.java

--- src/test/java/org/blackjack/Testcase.java
+++ src/test/java/org/blackjack/Testcase.java
@@ -1,8 +1,6 @@
-package org.blackjack;
-
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class Testcase {
