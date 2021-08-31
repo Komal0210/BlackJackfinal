82ade4064f51e3b8f54f9065e1e2843db4a04330
Komal0210 <89078874+Komal0210@users.noreply.github.com>
2021-08-31 23:26:31 +0100

    Update Testcase.java

--- src/test/java/org/blackjack/Testcase.java
+++ src/test/java/org/blackjack/Testcase.java
@@ -7,13 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class Testcase {
 
-    @Test
-    public double Bet(){
-        Testcase first = new Testcase();
-        first.Bet();
-        assertEquals(first.Bet(), "Total number of amount");
-        return 0;
+        @Test
+        public void testDeck(){
+        }
     }
-
-
-}
